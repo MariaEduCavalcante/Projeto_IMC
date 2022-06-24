f90b24a99c4c3b1fa2127b9367eaef36319ea56b
Maria Cavalcante <101808675+MariaEduCavalcante@users.noreply.github.com>
2022-06-24 12:08:29 -0300

    part3

--- app/src/main/java/com/example/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,17 +19,28 @@ public class MainActivity extends AppCompatActivity {
         edit1 = findViewById(R.id.peso);
         edit2 = findViewById(R.id.altura);
         resultado = findViewById(R.id.txtresultado);
+        fem = findViewById(R.id.btnfem);
+        masc = findViewById(R.id.btnmasc);
     }
 
     EditText edit1, edit2;
     TextView resultado;
+    RadioButton fem,masc;
 
     public void Calcula(View view) {
+
         int n1 = Integer.parseInt(edit1.getText().toString());
         int n2 = Integer.parseInt(edit2.getText().toString());
 
         int result = n1/(n2*n1);
 
+        if(fem == true){
+            result =
+        }
+        else{
+
+        }
         resultado.setText(Integer.valueOf(result).toString());
     }
+
 }
