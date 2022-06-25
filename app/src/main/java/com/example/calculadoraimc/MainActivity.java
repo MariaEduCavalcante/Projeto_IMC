a28833e15673a62c0e9da291f2aa56b84f94af70
Maria Cavalcante <101808675+MariaEduCavalcante@users.noreply.github.com>
2022-06-24 21:19:03 -0300

    Finalização

--- app/src/main/java/com/example/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.java
@@ -9,8 +9,13 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    
+    EditText edit1, edit2;
+    TextView resultado, resultadoNum;
+    RadioButton fem,masc;
+    double peso = 0.0;
+    double altura = 0.0;
+    boolean sexo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,28 +24,44 @@ public class MainActivity extends AppCompatActivity {
         edit1 = findViewById(R.id.peso);
         edit2 = findViewById(R.id.altura);
         resultado = findViewById(R.id.txtresultado);
+        resultadoNum = findViewById(R.id.txtresultado2);
         fem = findViewById(R.id.btnfem);
         masc = findViewById(R.id.btnmasc);
     }
 
-    EditText edit1, edit2;
-    TextView resultado;
-    RadioButton fem,masc;
-
     public void Calcula(View view) {
 
-        int n1 = Integer.parseInt(edit1.getText().toString());
-        int n2 = Integer.parseInt(edit2.getText().toString());
+        peso = Double.parseDouble(edit1.getText().toString());
+        altura = Double.parseDouble(edit2.getText().toString());
 
-        int result = n1/(n2*n1);
+        double result = peso/(altura*altura);
+        resultadoNum.setText(Double.valueOf(result).toString());
 
-        if(fem == true){
-            result =
+        if(fem.isChecked()){
+            if(result < 20){
+                    resultado.setText("Abaixo do peso normal");
+                } else if(result >= 20 && result <= 24.9) {
+                    resultado.setText("Normal");
+                } else if(result >= 25 && result <= 29.9){
+                    resultado.setText("Obesidade Leve");
+                } else if(result >= 30 && result <= 39.9) {
+                    resultado.setText("Obesidade Moderada");
+                } else if(result >= 43) {
+                    resultado.setText("Obesidade Mórbida");
+                }
         }
-        else{
-
+        else if(masc.isChecked() == false && fem.isChecked() == true){
+                if(result < 19){
+                    resultado.setText("Abaixo do peso normal");
+                } else if(result >= 19 && result <= 23.9) {
+                    resultado.setText("Normal");
+                } else if(result >= 24 && result <= 28.9){
+                    resultado.setText("Obesidade Leve");
+                } else if(result >= 29 && result <= 38.9) {
+                    resultado.setText("Obesidade Moderada");
+                } else if(result >= 39) {
+                    resultado.setText("Obesidade Mórbida");
+                }
         }
-        resultado.setText(Integer.valueOf(result).toString());
     }
-
 }
